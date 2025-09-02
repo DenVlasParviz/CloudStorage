ba179ffc3d9ec14224b12d2ea91a8a2cae9c679d
DenVlasParviz <vlas8366@gmail.com>
2025-09-02 14:58:54 +0300

    Added folder ids via routes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,17 +1,12 @@
-import Link from "next/link";
-import Drive from "~/app/Drive-contents";
-import {files as filesSchema, folders as foldersSchema} from "~/server/db/schema"
-import {db} from "~/server/db";
-import DriveContents from "~/app/Drive-contents";
 
 
-export default async function HomePage()
+export default  function HomePage()
 {
-  const files = await db.select().from(filesSchema)
-  const folders = await db.select().from(foldersSchema)
+
 
   return (
-<DriveContents files={files} folders={folders}/>
-
+<div>
+  Home page
+</div>
   );
 }
